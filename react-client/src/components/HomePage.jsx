404157c59c697612d76459f8ea00a17123d4cc56
misha <mishall.afzal@gmail.com>
2018-01-10 12:13:15 -0800

    created input fields for login and signup

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -14,6 +14,7 @@ class HomePage extends React.Component {
   }
 
   componentDidMount() {
+  };
 
 
   getBestSellersBooks() {
@@ -30,6 +31,7 @@ class HomePage extends React.Component {
     }).fail(function(err) {
       throw err;
     });
+
 
   }
 
