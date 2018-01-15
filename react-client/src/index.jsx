e44936b415949238d80be08b653c730cd6b1674c
misha <mishall.afzal@gmail.com>
2018-01-15 15:36:22 -0800

    trying to get page to rerender when a new search term is entered

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -71,8 +71,10 @@ class App extends React.Component {
   }
 
   handleSearch(query) {
-    this.setState({ view: 'Search', searchedBook: query });
-    this.renderView();
+    this.setState({ view: 'Search', searchedBook: query }, function () {
+      console.log(this.state.searchedBook);
+      this.renderView();
+    });
   }
 
   renderView() {
